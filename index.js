978328c9bf5abf961a1de4c4234e328b9d3a9417
Jason Brochu <jbrochu1@gmail.com>
2022-08-31 13:06:39 -0600

    commit tree c7adf882767c803d44f7de3b698ac8db12938bef

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const messages = []
+function writeCards(names, occasion) {
+    for (let i = 0; i < names.length; i ++) {
+       messages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+}
+return messages;
+}
+
+
+const int = 10;
+function countDown(int) {
+    let i = int;
+    while (i >= 0) {
+    console.log(i--);
+    
+    
+    }
+}
+
+
+
+ 
